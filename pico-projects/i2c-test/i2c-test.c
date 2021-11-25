feaff45359c6f5807236d03fc94e21ffababcfa8
tommyshem <1952659+tommyshem@users.noreply.github.com>
2021-11-25 18:25:56 +0000

    add serial debuging

--- pico-projects/i2c-test/i2c-test.c
+++ pico-projects/i2c-test/i2c-test.c
@@ -1,34 +1,29 @@
-#include <stdio.h>
+#include <stdio.h>  // for printf
 #include "pico/stdlib.h"
-#include "hardware/i2c.h"
 
-// I2C defines
-// This example will use I2C0 on GPIO8 (SDA) and GPIO9 (SCL) running at 400KHz.
-// Pins can be changed, see the GPIO function select table in the datasheet for information on GPIO assignments
-#define I2C_PORT i2c0
-#define I2C_SDA 8
-#define I2C_SCL 9
+#define GPIO_ON 1
+#define GPIO_OFF 0
+
+int main() {
+
+    stdio_init_all();  // setup stdio so printf can be used
+    printf("\nI2C-Test program\n");
+
+#ifndef PICO_DEFAULT_LED_PIN
+#warning blink example requires a board with a regular LED
+#else
+    const uint LED_PIN = PICO_DEFAULT_LED_PIN;
+    gpio_init(LED_PIN);
+    gpio_set_dir(LED_PIN, GPIO_OUT);
+    while (true) {
+        gpio_put(LED_PIN, GPIO_ON);
+        printf("Led turned on \n");
+        sleep_ms(250);
+        gpio_put(LED_PIN, GPIO_OFF);
+        printf("Led turned off \n");
+        sleep_ms(250);
+    }
+#endif
 
 
-
-int main()
-{
-
-    int a=10;
-    int b=20;
-    int c = a+b;
-    puts("output "+c);
-    // stdio_init_all();
-
-    // // I2C Initialisation. Using it at 400Khz.
-    // i2c_init(I2C_PORT, 400*1000);
-    
-    // gpio_set_function(I2C_SDA, GPIO_FUNC_I2C);
-    // gpio_set_function(I2C_SCL, GPIO_FUNC_I2C);
-    // gpio_pull_up(I2C_SDA);
-    // gpio_pull_up(I2C_SCL);
-
-    // puts("Hello, world!");
-
-    return 0;
 }
